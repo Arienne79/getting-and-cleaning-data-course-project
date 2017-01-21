c5ec558da6f909da3feb4f8d7ed532ad8cefad70
Arienne79 <arienne79@hotmail.com>
2017-01-21 11:07:52 +0100

    Add files via upload

---
+++ run_analysis.R
@@ -0,0 +1,51 @@
+# Reading trainings tables:
+x_train <- read.table("./data/UCI HAR Dataset/train/X_train.txt")
+y_train <- read.table("./data/UCI HAR Dataset/train/y_train.txt")
+subject_train <- read.table("./data/UCI HAR Dataset/train/subject_train.txt")
+
+# Reading testing tables:
+x_test <- read.table("./data/UCI HAR Dataset/test/X_test.txt")
+y_test <- read.table("./data/UCI HAR Dataset/test/y_test.txt")
+subject_test <- read.table("./data/UCI HAR Dataset/test/subject_test.txt")
+
+# Reading feature vector:
+features <- read.table('./data/UCI HAR Dataset/features.txt')
+
+# Reading activity labels:
+activityLabels = read.table('./data/UCI HAR Dataset/activity_labels.txt')
+
+colnames(x_train) <- features[,2] 
+colnames(y_train) <-"activityId"
+colnames(subject_train) <- "subjectId"
+
+colnames(x_test) <- features[,2] 
+colnames(y_test) <- "activityId"
+colnames(subject_test) <- "subjectId"
+
+colnames(activityLabels) <- c('activityId','activityType')
+
+# merge all data
+mrg_train <- cbind(y_train, subject_train, x_train)
+mrg_test <- cbind(y_test, subject_test, x_test)
+setAllInOne <- rbind(mrg_train, mrg_test)
+
+#reading column names
+colNames <- colnames(setAllInOne)
+
+#create vector for mean and standard deviation
+
+mean_and_std <- (grepl("activityId" , colNames) | 
+                   grepl("subjectId" , colNames) | 
+                   grepl("mean.." , colNames) | 
+                   grepl("std.." , colNames) 
+)
+setForMeanAndStd <- setAllInOne[ , mean_and_std == TRUE]
+
+#set activity names
+setWithActivityNames <- merge(setForMeanAndStd, activityLabels,
+                              by='activityId',
+                              all.x=TRUE)
+#create a tidy data set
+tidyData <- aggregate(. ~subjectId + activityId, setWithActivityNames, mean)
+tidyData <- tidyData[order(tidyData$subjectId, tidyData$activityId),]
+write.table(tidyData, "tidyData.txt", row.name=FALSE)
